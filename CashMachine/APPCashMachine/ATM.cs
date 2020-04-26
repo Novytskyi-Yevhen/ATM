ab928a33b30127ff11d6ce2c31c028938194faff
Евгений Новицкий <serverdog1@gmail.com>
2020-04-26 14:00:48 +0300

    Added a system of the number of banknotes in the machine. Added banknote replenishment system for collectors User choice is looped. Unit tests for a multiplicity of 10, for a negative / positive number, for a number up to 5000 have been added. =================================== Добавлена система количества банкнот в автомате. Добавлена система пополнения банкнот для инкасаторов Зациклен выбор пользователя. Добавлены юнит тесты на кратность 10, на отрицательное/положительное число, на число до  5000.

--- CashMachine/APPCashMachine/ATM.cs
+++ CashMachine/APPCashMachine/ATM.cs
@@ -6,64 +6,117 @@ namespace APPCashMachine
 {
     class ATM
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            int CheckerOut = 0;
-            while (CheckerOut==0)
+            Dictionary<int, int> DictionaryCountBanknote = AtmLibrary.CheckBanknoteInAtm(); // Возвращает словарь со значениями купюр в автомате
+            // Проверка купюр в устройстве. Если в АТМ присутствуют все 6 видо купюр в кол больше 10шт то возвращается число 6
+            int CheckWork = CheckWorkATM(DictionaryCountBanknote); 
+
+            if (CheckWork == 6)
             {
-                Console.Write("Выберите режим работы банкомата. 1 - Выдача наличных. 2 - Пополнение наличных (Только для инкасаторов).");
-                int SelectionMode = Convert.ToInt32(Console.ReadLine());
-                switch (SelectionMode)
+                Console.WriteLine("Приветствуем вас в банкомате.");
+                StartMenu();
+            }
+            else
+            {
+                Console.WriteLine("В автомате закончились банкноты. Ожидайте пополнения инкасаторами.");
+                Console.Write("Введите пароль инкасации(1111): ");
+                int Pass = Convert.ToInt32(Console.ReadLine());
+                if (Pass == 1111)
                 {
-                    case 1:
-                        Console.WriteLine("Вы выбрали режим работы - Выдача наличных.");
-                        AtmLibrary Value = new AtmLibrary(); // создание экземпляра класса библиотеки
+                    AtmLibrary.AddBanknoteATM();
+                    StartMenu();
+                }
+                else
+                {
+                    Console.WriteLine("Вы ввели неправильный пароль. Повторите попытку.");
+                    Main();
+                }
+            }
+            //Главный метод. Выбор режима работы
+            static void StartMenu()
+            {
+                int CheckerOut = 0;
+                while (CheckerOut == 0) // Главный цикл == Меню
+                {
+                    Console.Write("Выберите режим работы банкомата. 1 - Выдача наличных. 2 - Пополнение наличных (Только для инкасаторов).");
+                    int SelectionMode = Convert.ToInt32(Console.ReadLine());
+                    switch (SelectionMode)
+                    {
+                        case 1:
+                            Console.WriteLine("Вы выбрали режим работы - Выдача наличных.");
+                            AtmLibrary Value = new AtmLibrary(); // создание экземпляра класса библиотеки
 
-                        Dictionary<int, int> ValueCountMoney = Value.BanknoteDivision(); // Словарь со значениями Номинал:Количество
-
-
-
-                        foreach (KeyValuePair<int, int> keyValue in ValueCountMoney) // цикл вывода полученных данных
-                        {
-                            if (keyValue.Value > 0)
+                            Dictionary<int, int> ValueCountMoney = Value.BanknoteDivision(); // Словарь со значениями Номинал:Количество
+                            Console.WriteLine("Вам было выданно:");
+                            foreach (KeyValuePair<int, int> keyValue in ValueCountMoney) // цикл вывода полученных данных
                             {
-                                Console.WriteLine($"Количество купюр номиналом { keyValue.Key} = {keyValue.Value} шт.");
+                                if (keyValue.Value > 0)
+                                {
+                                    Console.WriteLine($"Купюр номиналом { keyValue.Key} = {keyValue.Value} шт.");
+                                }
                             }
-                        }
+                            break;
 
-                        break;
+                        case 2:
+                            Console.Write("Вы выбрали режим работы - Пополнение наличных (Только для инкасаторов). Введите код доступа (1111)");
+                            int SecurityCode = Convert.ToInt32(Console.ReadLine());
+                            if (SecurityCode == 1111)
+                            {
+                                AtmLibrary.AddBanknoteATM();
+                            }
+                            else
+                            {
+                                Console.WriteLine("У вас нет права доступа в этот раздел. Повторите попытку.");
+                            }
 
-                    case 2:
-                        Console.Write("Вы выбрали режим работы - Пополнение наличных (Только для инкасаторов).Введите код доступа (1111)");
-                        int SecurityCode = Convert.ToInt32(Console.ReadLine());
-                        if (SecurityCode==1111)
+
+                            break;
+
+                        default:
+                            Console.WriteLine("Вы выбрали не правильный вариант. Повторите попытку.");
+                            break;
+                    }
+                    
+                    int CheckWorkATM = 0;
+                    while (CheckWorkATM == 0)
+                    {
+                        Console.Write("Желаете продолжить работу с терминалом? 1-Да. 2-Нет.");
+                        CheckWorkATM = Convert.ToInt32(Console.ReadLine());
+
+                        if (CheckWorkATM == 1)
                         {
-                            AtmLibrary.AddBanknoteATM();
+                            Console.WriteLine("Хорошо, сейчас перенаправлю вас на главный экран.");
+                            CheckerOut = 0;
+                            CheckWorkATM = 1;
+                        }
+                        else if (CheckWorkATM == 2)
+                        {
+                            Console.WriteLine("Операции завершенны успешно. Хорошего дня.");
+                            CheckerOut = 1;
+                            CheckWorkATM = 1;
                         }
                         else
                         {
-                            Console.WriteLine("У вас нет права доступа в этот раздел. Повторите попытку.");
+                            Console.WriteLine("Вы выбрали не правильный ответ. Повторите ваш выбор.");
+                            CheckWorkATM = 0;
                         }
-
-
-                        break;
-
-                    default:
-                        Console.WriteLine("Вы выбрали не правильный вариант. Повторите попытку.");
-                        break;
+                    }
                 }
-                Console.Write("Желаете продолжить работу с терминалом? 1-Да. 2-Нет.");
-                int CheckWorkATM = Convert.ToInt32(Console.ReadLine());
-                if (CheckWorkATM == 1)
-                {
-                    CheckerOut = 0;
-                }
-                else if (CheckWorkATM == 2)
-                {
-                    CheckerOut = 1;
-                }
-
             }
-        }
+            // Проверяет наличие купюр в АТМ каждой купюры должно быть больше 10 шт
+            static int CheckWorkATM(Dictionary<int, int> DictionaryCountBanknote)
+            {
+                int CheckWorkATM = 0;
+                foreach (KeyValuePair<int, int> keyValue in DictionaryCountBanknote)
+                {
+                    if (keyValue.Value>10)
+                    {   
+                        CheckWorkATM++;
+                    }
+                }
+                return CheckWorkATM;
+            }
+        }   
     }
 }
--- CashMachine/LibraryATM/AtmLibrary.cs
+++ CashMachine/LibraryATM/AtmLibrary.cs
@@ -1,43 +1,83 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Xml;
+
 namespace LibraryATM
 {
     public class AtmLibrary
     {
-        public Dictionary<int, int> BanknoteDivision() // Метод деления суммы на купюры
+        public Dictionary<int, int> BanknoteDivision() // Метод возвращающий словарь с конечным результатом для пользователя
         {
             // new stage
 
-            int CheckOfAmount = 1; // Проверка суммы
-            int AmountEntered = 0; // Введенная сумма
-            while (CheckOfAmount > 0) // Цикл проверки суммы
+            XmlDocument XML = new XmlDocument(); // создание элемента XML
+            XML.Load("F://ReposGit//KeyLR//ATM//CashMachine//APPCashMachine//XML.xml"); // запись в элемент xml
+            XmlElement XMLValueCountBanknote = XML.DocumentElement; // разбор xml файла : Получаем корневой элемент
+
+            static int CheckAmountEntered(XmlElement XMLValueCountBanknote)
             {
-                Console.Write("Введите сумму кратную 10:");
-                //Console.WriteLine(-10%2);
-                AmountEntered = Convert.ToInt32(Console.ReadLine());
-                //Логика обработки 
 
-                CheckOfAmount = AmountEntered % 10;
+               
+                List<int> ValueMoney = new List<int>() { 500, 200, 100, 50, 20, 10 }; // Купюры 10 20 50 100 200 500
 
-                if (CheckOfAmount == 0 & AmountEntered > 0)
-                {
-                    Console.WriteLine("Успешная попытка. Продолжим");
-                    CheckOfAmount = 0;
-                }
-                else if (AmountEntered < 0)
-                {
-                    Console.WriteLine("Вы ввели отрицательное число. Повторите пожалуйста попытку.");
-                    CheckOfAmount = 1;
-                }
-                else
-                {
-                    Console.WriteLine("Вы ввели сумму не кратную 10. Повторите пожалуйста попытку.");
-                    CheckOfAmount = 1;
-                }
+                Dictionary<int, int> DictionaryCountBanknote = new Dictionary<int, int>() { };
                 
-            }
-            Console.WriteLine("Ваша сумма: " +AmountEntered);
+
+                foreach (XmlNode ValueCountBanknoteKey in XMLValueCountBanknote)
+                {
+                    foreach (int ListBanknoteKey in ValueMoney)
+                    {
+                        if (ValueCountBanknoteKey.Name == "BanknoteCountValue_" + Convert.ToString(ListBanknoteKey))
+                        {
+                            DictionaryCountBanknote.Add(ListBanknoteKey, Convert.ToInt32(ValueCountBanknoteKey.InnerText));
+                        }
+                    }
+                }
+                Console.WriteLine("Количество купюр в АТМ:");
+                foreach (KeyValuePair<int, int> keyValue in DictionaryCountBanknote) // цикл вывода полученных данных
+                {
+                    if (keyValue.Value > 0)
+                    {
+                        Console.WriteLine($" Номиналом { keyValue.Key} = {keyValue.Value} шт.");
+
+                    }
+                }
+                // На выходе словарь со значениями из XML
+
+
+
+                int AmountEntered = 0; // Введенная сумма
+                int CheckOfAmount = 1;
+
+                while (CheckOfAmount > 0) // check banknote cycle
+                {
+                    CheckOfAmount = AmountEntered % 10;
+                    Console.Write("Введите сумму кратную 10, но не больше 5000:");
+                    AmountEntered = Convert.ToInt32(Console.ReadLine());
+
+                    if (CheckMinusValue(AmountEntered) == true & CheckMultiplicity(AmountEntered) == true & CheckValueMax5000(AmountEntered)==true)
+                    {
+                        Console.WriteLine("Успешная попытка. Продолжим");
+                        CheckOfAmount = 0;
+                    }
+                    else if (CheckMinusValue(AmountEntered) == false)
+                    {
+                        Console.WriteLine("Вы ввели отрицательное число. Повторите пожалуйста попытку.");
+                        CheckOfAmount = 1;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Вы ввели сумму не кратную 10. Либо сумма больше 5000. Повторите пожалуйста попытку.");
+                        CheckOfAmount = 1;
+                    }
+                }
+                return AmountEntered;
+            } // на выходе число проверенное на +\- и кратность 10 и меньше 500
+
+            int AmountEntered = CheckAmountEntered(XMLValueCountBanknote);
+
+
+            Console.WriteLine("Ваша сумма: " + AmountEntered);
 
             // old stage
             List<int> ValueMoney = new List<int>() { 500, 200, 100, 50, 20, 10 }; // Купюры 10 20 50 100 200 500
@@ -49,13 +89,59 @@ namespace LibraryATM
                 ValueCountMoney.Add(i, AmountEntered / i); // i = key; AmountEntered/i = value
                 AmountEntered = AmountEntered % i;
             }
+
+            // На выходе словарь ValueCountMoney со значениями ключ значение
+
+            foreach (XmlNode ValueCountBanknoteKey in XMLValueCountBanknote)
+            {
+
+                foreach (int ValueMoneyKey in ValueMoney)
+                {
+                    if (ValueCountBanknoteKey.Name == "BanknoteCountValue_" + Convert.ToString(ValueMoneyKey))
+                    {
+                        // Старт оперции вычитания
+                        int MinusCount = ValueCountMoney[ValueMoneyKey];
+                        int LastCount = Convert.ToInt32(ValueCountBanknoteKey.InnerText);
+                        ValueCountBanknoteKey.InnerText = Convert.ToString(LastCount-MinusCount);
+
+                        XmlText AlCount = XML.CreateTextNode(ValueCountBanknoteKey.InnerText);
+                    }
+                }
+                XML.Save("F://ReposGit//KeyLR//ATM//CashMachine//APPCashMachine//XML.xml");
+
+            }
+
             return ValueCountMoney;
         }
-
-        public static void AddBanknoteATM()
+        public static void AddBanknoteATM()// Метод добавления купюр в ATM
         {
-            Console.Write("Введите сколько видов купюр вы хотите добавить: ");
-            int CountFaceValue = Convert.ToInt32(Console.ReadLine());
+            Dictionary<int, int> AddBanknoteDictionary = CheckBanknoteInAtm();
+            Console.WriteLine("В банкомате сейчас находятся:");
+            foreach (KeyValuePair<int, int> keyValue in AddBanknoteDictionary) // цикл вывода данных для инкасации перед добавлением валюты
+            {
+                Console.WriteLine($" Купюры номиналом { keyValue.Key} = {keyValue.Value} шт.");
+            }
+
+
+            int CountFaceValue = 0;
+            int CheckFaceValue = 0;
+            while (CheckFaceValue == 0)
+            {
+                Console.WriteLine("Максимум можно добавить 6 видов купюр. Номиналом 500,200,100,50,20,10");
+                Console.Write("Введите сколько видов купюр вы хотите добавить: ");
+                CountFaceValue = Convert.ToInt32(Console.ReadLine());
+                if (CountFaceValue<7 & CountFaceValue>0)
+                {
+                    CheckFaceValue = 1;
+                }
+                else
+                {
+                    Console.WriteLine("Вы выбрали не коректное количество видов купюр. Повторите попытку.");
+                    CheckFaceValue = 0;
+                }
+            }
+            
+
             List<int> ListBanknote = new List<int> { };
             int checker = 1;
             while (checker != CountFaceValue + 1)
@@ -66,40 +152,89 @@ namespace LibraryATM
                 checker++;
             }
 
-
-
-
             XmlDocument XML = new XmlDocument(); // создание элемента XML
-            XML.Load("C://Users//Kedr//source//repos//TestData//TestData//XML.xml"); // запись в элемент xml
+            XML.Load("F://ReposGit//KeyLR//ATM//CashMachine//APPCashMachine//XML.xml"); // запись в элемент xml
             XmlElement ValueCountBanknote = XML.DocumentElement; // разбор xml файла : Получаем корневой элемент
 
-
-
-
-
             // Цикл добавления значений в xml
-            foreach (XmlNode VCBKey in ValueCountBanknote)
+            foreach (XmlNode ValueCountBanknoteKey in ValueCountBanknote)
             {
-
-                foreach (int i in ListBanknote)
+                foreach (int ListBanknoteKey in ListBanknote)
                 {
-                    if (VCBKey.Name == "BanknoteCountValue_" + Convert.ToString(i))
+                    if (ValueCountBanknoteKey.Name == "BanknoteCountValue_" + Convert.ToString(ListBanknoteKey))
                     {
-                        Console.WriteLine("Количество купюр в автомате номиналом {0}: {1} шт. Добавьте купюры номиналом {0} в ATM.", i, VCBKey.InnerText);
+                        Console.WriteLine("Количество купюр в автомате номиналом {0}: {1} шт. Добавьте купюры номиналом {0} в ATM.", ListBanknoteKey, ValueCountBanknoteKey.InnerText);
                         Console.Write("Введите количество добавленых вами купюр в ATM: ");
                         int AddCount = Convert.ToInt32(Console.ReadLine());
-                        int LastCount = Convert.ToInt32(VCBKey.InnerText);
-                        VCBKey.InnerText = Convert.ToString(AddCount + LastCount);
+                        int LastCount = Convert.ToInt32(ValueCountBanknoteKey.InnerText);
+                        ValueCountBanknoteKey.InnerText = Convert.ToString(AddCount + LastCount);
 
 
-                        Console.WriteLine("Вы успешно загрузили купюры номиналом {0} в банкомат. Сейчас их в банкомате {1} шт. ", i, VCBKey.InnerText);
-                        XmlText AlCount = XML.CreateTextNode(VCBKey.InnerText);
+                        Console.WriteLine("Вы успешно загрузили купюры номиналом {0} в банкомат. Сейчас их в банкомате {1} шт. ", ListBanknoteKey, ValueCountBanknoteKey.InnerText);
+                        XmlText AlCount = XML.CreateTextNode(ValueCountBanknoteKey.InnerText);
                     }
                 }
-                XML.Save("C://Users//Kedr//source//repos//TestData//TestData//XML.xml");
-
+                XML.Save("F://ReposGit//KeyLR//ATM//CashMachine//APPCashMachine//XML.xml");
             }
+        } 
+        public static bool CheckMinusValue(int AmountEntered)// Возвращает True если число больше нуля и False если меньше
+        {
+            if (AmountEntered > 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public static bool CheckMultiplicity(int AmountEntered)// Возвращает True если число кратно 10 и False если нет
+        {
+            int CheckOfAmount = AmountEntered % 10;
 
+            if (CheckOfAmount == 0)
+            {
+                return true;
+            }
+            else
+            { 
+                return false;
+            }
+        }
+        public static bool CheckValueMax5000(int AmountEntered)// Возвращает True если число меньше 5001 и False если число больше
+        {
+            if (AmountEntered<5001)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public static Dictionary<int,int> CheckBanknoteInAtm()// Возвращает словарь со значениями купюр в автомате
+        {
+            XmlDocument XML = new XmlDocument(); // создание элемента XML
+            XML.Load("F://ReposGit//KeyLR//ATM//CashMachine//APPCashMachine//XML.xml"); // запись в элемент xml
+            XmlElement XMLValueCountBanknote = XML.DocumentElement; // разбор xml файла : Получаем корневой элемент
+
+            List<int> ValueMoney = new List<int>() { 500, 200, 100, 50, 20, 10 }; // Купюры 10 20 50 100 200 500
+
+            Dictionary<int, int> DictionaryCountBanknote = new Dictionary<int, int>() { };
+
+
+            foreach (XmlNode ValueCountBanknoteKey in XMLValueCountBanknote)
+            {
+                foreach (int ListBanknoteKey in ValueMoney)
+                {
+                    if (ValueCountBanknoteKey.Name == "BanknoteCountValue_" + Convert.ToString(ListBanknoteKey))
+                    {
+                        DictionaryCountBanknote.Add(ListBanknoteKey, Convert.ToInt32(ValueCountBanknoteKey.InnerText));
+                    }
+                }
+            }
+            // На выходе словарь со значениями из XML
+            return DictionaryCountBanknote;
         }
     }
 }
